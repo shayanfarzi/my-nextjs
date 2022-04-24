29833a4579c4ff2aec86d5a0a1e28af3d4e018cd
shayanfarzi <farzishayan@gmail.com>
2022-04-24 16:38:18 +0430

    link

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,11 @@
+import Link from "next/link";
 function News() {
-  return <h1>News Page</h1>;
+  return (
+    <>
+      <h1>News Page</h1>
+      <Link href='/news/first-news'>First News</Link>
+    </>
+  );
 }
 
 export default News;
